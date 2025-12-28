b5bec4d9ea4d9bc9f8b467ef682b4f8103491de1
Joel <junior.joelrosa@gmail.com>
2025-12-28 11:07:57 -0300

    Dia 1 - programa com validacao + gitignore

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Digite seu nome: ");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Digite sua idade: ");
+string textoIdade = Console.ReadLine();
+
+int idade;
+while (!int.TryParse(textoIdade, out idade))
+{
+    Console.Write("Idade inválida, tente novamente: ");
+    textoIdade = Console.ReadLine();
+}
+
+Console.WriteLine($"Olá, {nome}. Ano que vem você terá {idade + 1} anos.");
